591a2d0747eee5edb44a7fb6caefa2975af79bb5
Astrid <astridronja.novicky@gmail.com>
2018-05-26 10:12:09 +0100

    [Azi] init package.json, add path to server.js

--- phaser/server.js
+++ phaser/server.js
@@ -1,14 +1,16 @@
+const path = require("path");
 
-var express = require('express');
+var express = require("express");
+
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8000;
 
-// viewed at http://localhost:8080
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static(path.join(__dirname, "/assets")));
+// var fullPath = path.join(__dirname, "foo.js");
 
-app.get('/', function(req, res) {
-  res.sendfile('./assets/game.html');
+app.get("/", function(req, res) {
+  res.sendfile("./assets/game.html");
 });
 
 app.listen(port);
-console.log('Server launched on port:', port);
+console.log("Server launched on port:", port);
